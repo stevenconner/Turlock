aecae267db3b4b9064c174e734a3a5ebf870487f
stevenconner <steven.c.conner@gmail.com>
2016-10-19 15:09:29 -0700

    adding more list items

--- app/src/main/java/sigildesigns/turlock/EventsFragment.java
+++ app/src/main/java/sigildesigns/turlock/EventsFragment.java
@@ -1,10 +1,16 @@
 package sigildesigns.turlock;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -14,6 +20,45 @@ public class EventsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        View rootView = inflater.inflate(R.layout.entry_list, container, false);
+
+        // Create a list of some upcoming events in Turlock
+        final ArrayList<Entry> entries = new ArrayList<Entry>();
+        entries.add(new Entry("Main Street Live", "http://turlockdowntown.com/main-street-live", R.drawable.events_one));
+        entries.add(new Entry("Country Folk Art Craft Show","http://www.countryfolkart.com/1607_tur_nov/1607_overview.htm",R.drawable.events_two));
+        entries.add(new Entry("Turlock Festival of Lights", "http://www.turlockdowntown.com/content/festival-lights", R.drawable.events_three));
+
+
+
+
+        // Create an {@link EntryAdapter}, whose data source is a list of {@link entry}s. The
+        // Adapter knows how to create list items for each item in the list.
+        EntryAdapter adapter = new EntryAdapter(getActivity(), entries);
+
+        // Find the {@link GridView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link GridView} with the view ID called gridview
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
+
+        // Make the {@link GridView} use the {@link EntryAdapter} we created above, so that the
+        // {@link GridView} will display list items for each {@link Entry} in the list.
+        gridView.setAdapter(adapter);
+
+        // Set an onclick listener, this is going to open a specified URL when clicked by the user
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the position of the entry
+                Entry entry = entries.get(position);
+
+                // Create an intent to open a url
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(entry.getmLink()));
+                startActivity(browserIntent);
+            }
+        });
+
+        return rootView;
+    }
+    public EventsFragment(){
+        // Required empty public constructor
     }
 }
--- app/src/main/java/sigildesigns/turlock/RestaurantsFragment.java
+++ app/src/main/java/sigildesigns/turlock/RestaurantsFragment.java
@@ -1,10 +1,16 @@
 package sigildesigns.turlock;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -14,6 +20,40 @@ public class RestaurantsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_restaurants, container, false);
+
+        View rootView = inflater.inflate(R.layout.entry_list, container, false);
+
+        //Create a list of some of the good restaurants in Turlock
+        final ArrayList<Entry> entries = new ArrayList<Entry>();
+
+
+
+
+        // Create an {@link EntryAdapter}, whose data source is a list of {@link entry}s. The
+        // Adapter knows how to create list items for each item in the list.
+        EntryAdapter adapter = new EntryAdapter(getActivity(), entries);
+
+        // Find the {@link GridView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link GridView} with the view ID called gridview
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
+
+        // Make the {@link GridView} use the {@link EntryAdapter} we created above, so that the
+        // {@link GridView} will display list items for each {@link Entry} in the list.
+        gridView.setAdapter(adapter);
+
+        // Set an onclick listener, this is going to open a specified URL when clicked by the user
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the position of the entry
+                Entry entry = entries.get(position);
+
+                // Create an intent to open a url
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(entry.getmLink()));
+                startActivity(browserIntent);
+            }
+        });
+
+        return rootView;
     }
 }
